46e0fdd3fc8568972f47ec43fde038d8bc4b5af5
Hariharan Srinath <srinathh@gmail.com>
2025-03-09 18:35:09 +0800

    without random global variables

--- src/app/app.py
+++ src/app/app.py
@@ -1,56 +1,59 @@
 from dotenv import load_dotenv
 import os
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from typing import Annotated
-from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-
+import logging
+from pydantic import BaseModel
+from src.utils.singleton import Singleton
 load_dotenv()
-assert os.environ.get("OPENAI_API_KEY") != ""
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
 
-class State(TypedDict):
+class Globals(metaclass=Singleton):
+    llm: BaseLanguageModel
+
+    def __init__(self):
+        self.llm = ChatOpenAI(model="gpt-4o")
+
+class State(BaseModel):
     messages: Annotated[list, add_messages]
 
-graph_builder = StateGraph(State)
-llm = ChatOpenAI(model="gpt-4o-mini")
-
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    for message in state.messages:
+        logging.debug(f"{type(message)} : {message}")
 
-graph_builder.add_node("chatbot", chatbot)
+    return {"messages": [Globals().llm.invoke(state.messages)]}
 
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
-
-graph = graph_builder.compile()
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages":[{"role":"user","content":user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-#
-# from utils.sample import sample_util
 def main():
+    # llm = ChatOpenAI(model="gpt-4o-mini")
+    assert os.environ.get("OPENAI_API_KEY") != ""
+    graph_builder = StateGraph(State)
+    graph_builder.add_node("chatbot", chatbot)
+    graph_builder.add_edge(START, "chatbot")
+    graph_builder.add_edge("chatbot", END)
+    graph = graph_builder.compile()
 
-    while True:
+    done = False
+
+    while not done:
+        user_input = ""
         try:
             user_input = input("User: ")
             if user_input.lower() in ["q", "quit", "exit"]:
                 print("Goodbye")
                 break
-
-            stream_graph_updates(user_input)
         except:
-            # if input() is missing
             user_input = "What do you know about LangGraph?"
-            print(f"User: {user_input}")
-            stream_graph_updates(user_input)
-            break
 
+        result = graph.invoke({"messages":HumanMessage(user_input)})
+        print(result["messages"][-1].content)
 
-    pass
 
 if __name__ == "__main__":
     main()
